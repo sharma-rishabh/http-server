18218d6db5b4a6b38da54e304d386e2ee536c19e
Risabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2022-07-07 17:48:00 +0530

    Removed unecceary log from notFoundHandler.

--- src/notFoundHandler.js
+++ src/notFoundHandler.js
@@ -1,7 +1,6 @@
 const html = (body) => `<h1>${body}</h1>`;
 
 const notFoundHandler = (_, response) => {
-  console.log('inside Not found');
   response.statusCode = 404;
   response.setHeader('Content-Type', 'text/html');
   response.end(html('Sorry!! We couldn\'t find the page you\'re looking for'));
